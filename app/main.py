7e846356222c83b3330a98b4392bb7e0fe9fc71a
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2024-11-16 11:54:41 -0600

    Minor changes

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from app.core import app
 from app.presentation.api import root
 from app.presentation.api.auth import auth
+from app.presentation.api.factory import factory
 from app.presentation.api.travel_match_network_system import travels
 from app.presentation.api.user_management_system import user_management_system
 
 app.include_router(root)
+app.include_router(factory)
 app.include_router(auth)
+
 app.mount("/user_management_system", user_management_system)
 app.mount("/travel_match_network_system", travels)
--- app/presentation/api/__init__.py
+++ app/presentation/api/__init__.py
@@ -1,9 +1,5 @@
 from fastapi import APIRouter
 
-from app.core.dependencies import DependUserManagementCase
-from app.domain.factory.users import create_dummy_user_data
-from app.presentation.schemes import UserRequest
-
 root = APIRouter(
     tags=["Root"]
 )
@@ -14,20 +10,3 @@ async def index():
     return {
         "msg": "Hello World"
     }
-
-
-@root.get('/factory/user')
-async def factory_user(user_case: DependUserManagementCase):
-    status = True
-
-    for _ in range(30):
-        data = create_dummy_user_data()
-
-        await user_case.create(
-            UserRequest(**data),
-        )
-
-    return {
-        "status": "success" if status else "failure",
-    }
-
