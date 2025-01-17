3b6dc5317fd5db0f54ce9276fd4857ca47d69cd3
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2025-01-16 21:19:59 -0600

    First implementation of StatusResponse

--- app/core/types.py
+++ app/core/types.py
@@ -2,9 +2,21 @@ import typing
 import uuid
 
 from fastapi import UploadFile
+from enum import Enum
 
 UserType = typing.TypeVar("UserType")
 
 DocumentRequest = typing.TypeVar("DocumentRequest", UploadFile, bytes)
 UUID = typing.TypeVar("UUID", str, uuid.UUID)
 Token = typing.TypeVar("Token", str, bytes)
+
+
+class Status(str, Enum):
+    success = 'success'
+    failure = 'failure'
+
+
+class Gender(str, Enum):
+    male = 'male'
+    female = 'female'
+    other = 'other'
