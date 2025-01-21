91dcfbe57d4256f8b2e5f9d26a8fc9d2f57f38b8
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2025-01-20 21:13:26 -0600

    Implemented StatusQuery type

--- app/core/types.py
+++ app/core/types.py
@@ -1,14 +1,16 @@
 import typing
 import uuid
-
-from fastapi import UploadFile
 from enum import Enum
 
+from fastapi import UploadFile
+
 UserType = typing.TypeVar("UserType")
+M = typing.TypeVar('M')
 
 DocumentRequest = typing.TypeVar("DocumentRequest", UploadFile, bytes)
 UUID = typing.TypeVar("UUID", str, uuid.UUID)
 Token = typing.TypeVar("Token", str, bytes)
+StatusQuery = typing.Tuple[bool, typing.LiteralString]
 
 
 class Status(str, Enum):
