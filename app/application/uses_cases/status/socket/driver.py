eae8727ed797a2ad9d18c2d15a2628868eac9baa
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2025-01-27 21:05:27 -0600

    Minor changes

--- app/application/uses_cases/status/socket/driver.py
+++ app/application/uses_cases/status/socket/driver.py
@@ -4,24 +4,25 @@ from app.application.uses_cases.status.socket.events import EventsSocketNotifica
 from app.core.manager.socket import SessionSocket
 from app.core.types import Status, UUID
 from app.presentation.schemes.status import RidesStatus, PassengerRideStatus, PassengerProfile
-from app.presentation.schemes.websocket import StatusResponseWebSocket
+from app.presentation.schemes.websocket import StatusResponseWebSocket, StatusMessageWebSocket
 
 
 class DriverEventsSocket(EventsSocketNotifications):
     async def notification(self, session: SessionSocket, uuid: UUID):
         async def get_ride_status():
+            status, schedule = await self.schedule_service.get(uuid)
             rides = await self.ride_service.get_all_rides(schedule)
             passengers = await schedule.passengers.all()
 
             rides_response = []
 
             for r, p in zip(rides, passengers):
-                user = PassengerProfile(
-                    code=p.code,
-                    firstname=p.firstname,
-                    maternal_surname=p.maternal_surname,
-                    paternal_surname=p.paternal_surname,
-                )
+                user = PassengerProfile(**{
+                    'code': p.code,
+                    'firstname': p.firstname,
+                    'maternalSurname': p.maternal_surname,
+                    'paternalSurname': p.paternal_surname,
+                })
 
                 ride = PassengerRideStatus(
                     valid=r.validate,
@@ -39,12 +40,18 @@ class DriverEventsSocket(EventsSocketNotifications):
                 status=Status.success,
             ))
 
+        async def tracking_user_gps(): ...
+
         await session.get_user_from_token(self.auth_service)
 
-        status, schedule = await self.schedule_service.get(uuid)
+        await session.send_model(StatusMessageWebSocket(
+            message="Token is valid.",
+            status=Status.success
+        ))
 
         while True:
-            await get_ride_status()
+            await get_ride_status()     # OUT
+            await tracking_user_gps()   # IN
 
             await asyncio.sleep(5)
 
