4c1f52c039700d436e7aa9905ac2e0696db01103
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2025-02-22 21:27:14 -0600

    Minor changes

--- app/application/uses_cases/status/cache/tracking.py
+++ app/application/uses_cases/status/cache/tracking.py
@@ -1,25 +1,25 @@
 from typing import List
 
 from app.core.dependencies.depends.database.redis import CacheSession
-from app.core.types import UUID
-from app.domain.types import LocationData, TrackingRecordData
+from app.core.types import UUID, UserCode
+from app.domain.types import TrackingRecordData
 from app.infrastructure.repository.cache.tracking import CacheTrackingRepository
 from app.infrastructure.repository.travels.tracking import StaticTrackingRepository
 
 
 class TrackingService:
-    async def record_to_cache(self, cache: CacheSession, uuid: UUID, record: TrackingRecordData) -> bool:
-        status = await CacheTrackingRepository.record(cache, uuid, record)
+    async def record_to_cache(self, cache: CacheSession, use_code: UserCode, record: TrackingRecordData) -> bool:
+        status = await CacheTrackingRepository.record(cache, use_code, record)
 
         return status
 
-    async def get_records_from_cache(self, cache: CacheSession, uuid: UUID) -> List[LocationData]:
+    async def get_records_from_cache(self, cache: CacheSession, use_code: UserCode) -> List[TrackingRecordData]:
         return []
 
-    async def save_data_to_database_from_driver(self, cache: CacheSession, uuid: UUID) -> bool:
+    async def save_data_to_database_from_driver(self, cache: CacheSession, use_code: UserCode, uuid: UUID) -> bool:
         return False
 
-    async def save_data_to_database_from_passenger(self, cache: CacheSession, uuid: UUID) -> bool:
+    async def save_data_to_database_from_passenger(self, cache: CacheSession, use_code: UserCode, uuid: UUID) -> bool:
         return False
 
     async def get_records_from_data(self, uuid: UUID) -> List[TrackingRecordData]:
--- app/infrastructure/repository/cache/tracking.py
+++ app/infrastructure/repository/cache/tracking.py
@@ -1,15 +1,19 @@
 from typing import List
 
 from app.core.dependencies.depends.database.redis import CacheSession
-from app.core.types import UUID
+from app.core.types import UUID, UserCode
 from app.domain.types import TrackingRecordData
 
 
 class CacheTrackingRepository:
     @staticmethod
-    async def record(cache: CacheSession, uuid: UUID, record: TrackingRecordData) -> bool:
+    async def record(cache: CacheSession, use_code: UserCode, record: TrackingRecordData) -> bool:
         return False
 
     @staticmethod
-    async def get_record(cache: CacheSession, uuid: UUID) -> List[TrackingRecordData]:
+    async def get_record(cache: CacheSession, use_code: UserCode) -> List[TrackingRecordData]:
         return []
+
+    @staticmethod
+    async def user_is_record(cache: CacheSession, use_code: UserCode):
+        pass
