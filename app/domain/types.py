55f33a7e7138d7dbe050421f0ddc9e0b8a73f73c
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2025-03-10 21:43:47 -0600

    Implement methods for serialize data

--- app/domain/types.py
+++ app/domain/types.py
@@ -1,14 +1,27 @@
+import abc
 import dataclasses
+import json
+
+
+class DataSerialize:
+    @property
+    @abc.abstractmethod
+    def dump(self) -> dict: ...
+
+    @property
+    @abc.abstractmethod
+    def serialize(self) -> str: ...
 
 
 @dataclasses.dataclass
-class LocationData:
+class LocationData(DataSerialize):
     latitude: float
     longitude: float
 
     @property
-    def dump(self):
-        return {
-            'latitude': self.latitude,
-            'longitude': self.longitude,
-        }
+    def dump(self) -> dict:
+        return self.__dict__
+
+    @property
+    def serialize(self) -> str:
+        return json.dumps(self.dump)
