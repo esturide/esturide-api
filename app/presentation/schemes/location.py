a95d04c0d5940d6b9e1e3f4ea4de66c8b06e4d12
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2025-03-10 22:04:13 -0600

    Rename model for data address location

--- app/presentation/schemes/location.py
+++ app/presentation/schemes/location.py
@@ -1,11 +1,7 @@
 from pydantic import BaseModel, Field
 
 
-class FoundLocation(BaseModel):
+class DataAddressLocation(BaseModel):
     address: str = Field(..., title="Address", alias='address')
     longitude: float = Field(0, title="Longitude", alias='longitude')
     latitude: float = Field(0, title="Latitude", alias='latitude')
-
-
-class SearchLocation(FoundLocation):
-    ...
