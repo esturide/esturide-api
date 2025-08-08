2ccb7d7e24e68e5901a5dd3f6cdc003948b29503
SealtielFreak <Die.Seal_Valderrama99@outlook.com>
2025-08-07 21:37:32 -0600

    Fix Graphql

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ from app.presentation.api.auth import auth
 from app.presentation.api.health import health
 from app.presentation.api.v1.user_management import user_management_v1
 from app.presentation.api.v1.travel_match_network import travels_match_network_v1
-from app.presentation.api.v2 import get_graphql_route
 
 for _app in [app, user_management_v1, travels_match_network_v1]:
     _app.add_exception_handler(ResponseException, custom_http_exception_handler)
@@ -26,7 +25,6 @@ for _app in [app, user_management_v1, travels_match_network_v1]:
 app.include_router(root)
 app.include_router(auth)
 app.include_router(health)
-app.include_router(get_graphql_route(), prefix="/graphql")
 
 app.add_middleware(
     CORSMiddleware,
